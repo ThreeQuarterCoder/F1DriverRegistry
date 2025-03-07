ee8495fb3d5eb88d620a38a3495debc2ddd13ad1
fci_2151 <fci_2151@fci-2151s-MacBook-Pro.local>
2025-03-07 10:26:25 +0530

    Making initial commit to data models

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
+import uvicorn
+
 
 app = FastAPI(title="Pro Rider")
 
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="localhost", port="9500", debug=True)
+    uvicorn.run(app, host="localhost", port=9500, reload=True, access_log=True)
 
     
---
+++ firebase-login.js
@@ -0,0 +1,23 @@
+<script src="https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/11.4.0/firebase-auth.js"></script>
+<script src="https://www.gstatic.com/firebasejs/11.4.0/firebase-firestore.js"></script>
+
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyD1f3xPSu8nDWaPO-6r29MrDGo7e_tIfVU",
+    authDomain: "f1-driver-registry.firebaseapp.com",
+    projectId: "f1-driver-registry",
+    storageBucket: "f1-driver-registry.firebasestorage.app",
+    messagingSenderId: "425340909294",
+    appId: "1:425340909294:web:612a49c0471636a2ef65ff"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
